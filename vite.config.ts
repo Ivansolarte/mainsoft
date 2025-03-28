b85a38fcc5316309a6448f409bacbf8cb3193479
Ivan <chaca360@gmail.com>
2025-03-28 13:40:00 -0500

    Fix package.json syntax 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import NodePolyfillPlugin from 'vite-plugin-node-polyfill';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    NodePolyfillPlugin()  // Ahora usamos este plugin específico para Vite
+  ],
   define: {
-    'global': 'window', // Esto ayuda a solucionar problemas con algunos módulos
-  },
-  optimizeDeps: {
-    include: ['crypto-browserify'],
+    'global': 'window', // Esto puede ayudar a resolver algunos problemas con la compatibilidad de módulos
   },
 });
